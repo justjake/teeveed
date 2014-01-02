b2cc3c22bd1673f0e6d260906b79645905a41a2e
Jake Teton-Landis <just.1.jake@gmail.com>
2014-01-01 22:49:20 -0800

    wrapper objects for Wit queries

--- lib/teevee/wit.rb
+++ lib/teevee/wit.rb
@@ -6,18 +6,53 @@ module Teevee
   # Wit performs all our natural language processing tasks
   class Wit < Faraday::Connection
 
-    # A specific Wit intent.
-    # TODO: is this needed?
-    class Intent
-      attr_accessor :type     # string
-      attr_accessor :entities # Array<Entity>
-    end
 
     # A Wit entity.
     # TODO: is this needed?
     class Entity
-      attr_accessor :intent # Intent
-      attr_accessor :type   # string
+      attr_reader :type,      # sym
+                  :start,     # int
+                  :end,       # int
+                  :value,     # any!
+                  :body,      # string
+                  :suggested  # bool
+
+      def initialize(type, json_hash)
+        @type = type.to_sym
+        @start = json_hash["start"]
+        @end = json_hash["end"]
+        @value = json_hash["value"]
+        @body = json_hash["body"]
+        @suggested = json_hash["suggested"]
+      end
+    end
+
+    # A specific Wit intent.
+    # TODO: is this needed?
+    class Intent
+      attr_reader :type,        # sym
+                  :confidence,  # float
+                  :entities     # Hash<EntityType(Sym), Entity>
+
+      def initialize(json_hash)
+        @type = json_hash["intent"].to_sym
+        @confidence = json_hash["confidence"]
+        @entities = {}
+        json_hash["entities"].each do |k, v|
+          ent = Entity.new(k, v)
+          @entities[ent.type] = ent
+        end
+      end
+    end
+
+    class Query
+      attr_reader :body
+      attr_reader :outcome
+
+      def initialize(json_hash)
+        @body = json_hash["msg_body"]
+        @outcome = Intent.new(json_hash["outcome"])
+      end
     end
 
     # Create a new Wit api with the given Oauth access token, as seen
@@ -28,12 +63,21 @@ module Teevee
       })
     end
 
+    def message(query)
+      resp = self.get('/message', :q => query)
+      resp.body
+    end
+
     # Send user input data to Wit.ai to recieve a parsed intent
     # should be using Faraday middleware but its inexplicably broken
     # and i'm tired of fighting this problem here
-    def message(query)
-      resp = self.get('/message', :q => query)
-      JSON.parse(resp.body)
+    def message_json(query)
+      JSON.parse(message(query))
+    end
+
+    # Fully wrapped and hopefully fixture-safe for future subclassing
+    def query(query)
+      Query.new(message_json(query))
     end
   end
 end
