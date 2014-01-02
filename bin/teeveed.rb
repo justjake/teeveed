a5ea5bc964b06c97cde42ae873a84f4c45ce4dbd
Jake Teton-Landis <just.1.jake@gmail.com>
2014-01-01 21:22:24 -0800

    tests for indexing movie and television episode

--- bin/teeveed.rb
+++ bin/teeveed.rb
@@ -3,6 +3,13 @@
 # https://github.com/justjake/teeveed
 # GNU GPLv3
 
+# try to run in cmd.exe
+if ARGV[0] == 'launch'
+  `start "Teeveed Console" /max cmd.exe /k jruby '#{__FILE__}' cli`
+  puts "finished CLI session"
+  exit 0
+end
+
 # config
 WIT_ACCESS_TOKEN = ENV['WIT_ACCESS_TOKEN']
 TEEVEED_HOME = Pathname.new(__FILE__).parent.parent.realpath + 'arena'
@@ -24,25 +31,52 @@ DataMapper.setup(:default, 'sqlite::memory:')
 DataMapper.finalize
 DataMapper.auto_migrate!
 
+TEST_PATHS = [
+    'Television/Game of Thrones/Season 02/Game of Thrones - S02E03 - What is Dead May Never Die.mp4',
+    'Movies/Zoolander (2001).avi'
+]
+
 ### some siple tests
+root = Teevee::Library::Root.new (TEEVEED_HOME+'library').to_s, {
+    'Television' => Teevee::Library::Episode,
+    'Movies' => Teevee::Library::Movie
+}
 cli = Teevee::Daemon::CLI.new
-path_to_index = TEEVEED_HOME + cli.data
-row = cli.index(path_to_index)
-p "indexed #{path_to_index} "
-cli.store = row
 
-
-# start the webserver for the remote in a thread
-web = Thread.new do
-  Teevee::Daemon::Remote.run!
-end
-
-# start pry-remote in a thread
-debug = Thread.new do
-  while true do
-    cli.interact!
+TEST_PATHS.each do |path|
+  path = cli.root.pathname + path
+  begin
+    row = root.index_path(path)
+    if row
+      puts "SUCCESS: indexed #{path} as \n#{row.inspect}"
+      cli.store = row
+    else
+      puts "FAILURE: couldn't index #{path}"
+    end
+  rescue => err
+    puts "FAILURE: exeption #{err}"
+    puts err.backtrace
   end
 end
 
-# wait for our server (forever)
-[web, debug].each {|t| t.join}
+if ARGV[0] == 'cli'
+  binding.pry
+end
+
+### daemonize if env is right
+if ENV['TEEVEED_DAEMONIZE'] == 'YES'
+  # start the webserver for the remote in a thread
+  web = Thread.new do
+    Teevee::Daemon::Remote.run!
+  end
+
+  # start pry-remote in a thread
+  debug = Thread.new do
+    while true do
+      cli.interact!
+    end
+  end
+
+  # wait for our server (forever)
+  [web, debug].each {|t| t.join}
+end
