3d607a2ffd5dc7186bac9a8224b7b16367b4b636
Jake Teton-Landis <just.1.jake@gmail.com>
2014-01-07 23:37:54 -0800

    play TV seasons with VLC

--- lib/teevee/daemon/intent_controller.rb
+++ lib/teevee/daemon/intent_controller.rb
@@ -17,6 +17,17 @@ module Teevee
         @app = application
       end
 
+      # add wait-time while starting VLC server.
+      # this makes things very slow, the web ui should return first.
+      def vlc
+        _vlc = @app.vlc
+        if _vlc.server.stopped?
+          _vlc.server.start
+          sleep 2 # wait for things to boot
+        end
+        _vlc
+      end
+
       def handle_intent(intent)
         if self.respond_to? intent.type
           return self.send(intent.type, intent)
@@ -31,7 +42,9 @@ module Teevee
             :episode => :episode_num,
         }
 
-        query = {}
+        query = {
+            :order => [:season.asc, :episode_num.asc]
+        }
         name_mapping.each do |wit, db|
           if intent.entities.include? wit
             query[db] = intent.entities[wit].value
@@ -42,13 +55,14 @@ module Teevee
 
         # fuzzy matching stuff
         if intent.entities.include? :title
-          # title => search "show"
           episodes = results.search(intent.entities[:title].value, :search_indexes => [:show])
+          log "found #{episodes.length} show=#{intent.entities[:title].value}"
           results = episodes if episodes.length > 0
         end
 
         if intent.entities.include? :episode_name
           episodes = results.search(intent.entities[:episode_name].value, :search_indexes => [:title])
+          log "found #{episodes.length} title=#{intent.entities[:episode_name].value}"
           results = episodes if episodes.length > 0
         end
 
@@ -57,6 +71,7 @@ module Teevee
         end
 
         episode = results[0]
+
         if episode.attributes.include? :season and episode.attributes.include? :episode_num
           # we have a well-frormatted episode on our hands
           # build a playlist of the following episodes in the season
@@ -65,21 +80,24 @@ module Teevee
             :episode_num.gt => episode.episode_num,
             :order => [:episode_num.asc]
           ).to_a
-          playlist = season.map{ |f| @app.root.pathname + f.relative_path}
 
+          playlist = season.map{ |f| (@app.root.pathname + f.relative_path).to_s }
 
-          # do vlc things
-          # vlc = @app.vlc
-          # vlc.start unless vlc.running?
-          # vlc.clear # clear okaylist
-          # vlc.play((@app.root.pathname + episode.relative_path).to_s)
-          # playlist.each{|f| vlc.add_to_playlist(f) }
+          Thread.new do
+            vlc.connect
+            vlc.clear # clear pkaylist
+            vlc.play((@app.root.pathname + episode.relative_path).to_s)
+            playlist.each{|f| vlc.add_to_playlist(f) }
+          end
 
           return playlist
         end
 
         return episode
+      end # query_episode
 
+      def log(*things)
+        puts "IntentController: #{things.join(': ')}"
       end
 
     end
