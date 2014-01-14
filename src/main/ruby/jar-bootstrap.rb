8e0f1565f09ab37f7f630a5b24040480f0bf6a87
Jake Teton-Landis <just.1.jake@gmail.com>
2014-01-13 18:21:37 -0800

    try to get JRUBY classpath correct

--- src/main/ruby/jar-bootstrap.rb
+++ src/main/ruby/jar-bootstrap.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
 # this file is run by JRuby in the compiled jar.
 # it is expected to be in the root of the jar, next to teeveed.rb
+require 'pathname'
+$CLASSPATH << Pathname.new(__FILE__).parent.realpath.to_s
+
 require 'teeveed'
 
