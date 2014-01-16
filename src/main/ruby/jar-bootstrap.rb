849c1a79de9847e493d9e5f86127f9dd92105fe1
Jake Teton-Landis <just.1.jake@gmail.com>
2014-01-16 15:16:25 -0800

    use new entrypoint Teevee::Daemon.main()

--- src/main/ruby/jar-bootstrap.rb
+++ src/main/ruby/jar-bootstrap.rb
@@ -1,8 +1,6 @@
+#!/usr/bin/env ruby
 # -*- encoding : utf-8 -*-
 # this file is run by JRuby in the compiled jar.
 # it is expected to be in the root of the jar, next to teeveed.rb
-require 'pathname'
-$CLASSPATH << Pathname.new(__FILE__).parent.realpath.to_s
-
-require 'teeveed'
-
+require 'teevee/daemon'
+Teevee::Daemon.main(ARGV)
